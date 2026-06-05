2bfb61eb30e54645a4f3d8997cd39ee0fad51cd3
rissu <verzonillacris00@gmail.com>
2026-06-05 12:36:18 +0800

    fix dashboard route

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/emulators', function () {
 });
 
 Route::get('/dashboard', function () {
-    return redirect('/dashboard');
+    return view('/dashboard');
 })->middleware('auth');
 
 Route::get('/profile', function () {
