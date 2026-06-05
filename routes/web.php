5a8783ea17664e121ef0c7ac221b92d202a7896f
rissu <verzonillacris00@gmail.com>
2026-06-05 12:41:49 +0800

    fix dashboard sidebar link

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/emulators', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('/dashboard');
+    return view('dashboard');
 })->middleware('auth');
 
 Route::get('/profile', function () {
